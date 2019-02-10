de439b8869ff7671084c5ee10c3d8c3313225649
JacobGil64 <44482108+JacobGil64@users.noreply.github.com>
2019-02-09 16:34:45 -0800

    Add files via upload

---
+++ DriveSubsystem.java
@@ -0,0 +1,37 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class DriveSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  // WPI_TalonSRX vs Talon ?
+  public Talon leftMotor = new Talon(RobotMap.leftTalonPort);
+  public Talon rightMotor = new Talon(RobotMap.rightTalonPort);
+
+  DifferentialDrive drive = new DifferentialDrive(leftMotor, rightMotor);
+
+  public void manualDrive(double leftSpeed, double rightSpeed) {
+    drive.tankDrive(leftSpeed, rightSpeed);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
